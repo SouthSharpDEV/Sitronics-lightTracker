b218b956c055a0a581e4a7b3f43c1026594290c6
Constantine Oganesyants <53556592+OganesK@users.noreply.github.com>
2023-06-15 02:33:00 +0300

    add upload/get functions

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from os import listdir
+from os.path import isfile, join
+import json
 import aiofiles
 
 
@@ -6,7 +10,16 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return 'anus'
+    onlyfiles = [f for f in listdir('./resultMeta') if isfile(join('./resultMeta', f))]
+    data = []
+    for i in onlyfiles:
+        with open('./resultMeta/{}'.format(i)) as file:
+            data.append(json.load(file))
+    return data
+
+@app.get('/media/')
+async def getMedia(imageName:str):
+    return FileResponse(path='./raw/{}'.format(imageName), filename=imageName, media_type='image/jpeg')
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
@@ -15,4 +28,3 @@ async def create_upload_file(file: UploadFile):
         await out_file.write(content)  # async write
 
     return {"Result": "OK"}
-    # return {"filename": file.filename}
